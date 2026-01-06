daaeff25862bbdb2ae4304d917669fda91649306
Juan Carlos Garcia Solis <jgarcias@gmu.edu>
2026-01-06 18:11:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/**Auto Updates Year*/
+document.addEventListener("DOMContentLoaded", () => { 
+    const yearSpan = document.getElementById("year");
+
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+});
